a432eee859207573cbb4d592f56d335688f15e11
ZhouxiangZhan <3150102092@zju.edu.cn>
2017-09-04 16:14:49 +0800

    modal init

--- JS/p3_detailsinfo_FieldClick.js
+++ JS/p3_detailsinfo_FieldClick.js
@@ -1,9 +1,5 @@
 $(document).ready(function(){
-    $("#videoplayer").on(
-        "timeupdate",
-        function(event){
-            onTrackedVideoFrame(this.currentTime, this.duration);
-        });
+    updateTime()
 
     $("#pause").on("click", function(){
         let player = $("#videoplayer")[0];
@@ -14,36 +10,48 @@ $(document).ready(function(){
             player.pause()
         }
     })
+
     $("#field").on("click",function(){
         let player = $("#videoplayer")[0];
-        if(player.paused){
-            setTimeout(player.play(), 500);
-        }
-        else{
-            player.pause()
-        }
-
-        mouseMove(event)
-
-        function mouseMove(ev)
-        {
-            Ev= ev || window.event;
-            var mousePos = mouseCoords(ev);
-            $("#xxx").text (((mousePos.x-816)/6.8).toFixed(1));
-            $("#yyy").text (((mousePos.y-204.5)/4.5).toFixed(1));
-        }
-        function mouseCoords(ev)
-        {
-            if(ev.pageX || ev.pageY){
-                return {x:ev.pageX, y:ev.pageY};
-            }
-            return{
-                x:ev.clientX + document.body.scrollLeft - document.body.clientLeft,
-                y:ev.clientY + document.body.scrollTop - document.body.clientTop
-            };
-        }
+        player.pause();
+        let coor = mouseMove(event);
+        popModal()
     })
+
 });
+
+function popModal()
+{
+    $('#collector').modal({backdrop: 'static', keyboard: false})
+}
+
+function updateTime()
+{
+    $("#videoplayer").on(
+        "timeupdate",
+        function(event){
+            onTrackedVideoFrame(this.currentTime, this.duration);
+        });
+}
+
+function mouseMove(ev)
+{
+    Ev= ev || window.event;
+    var mousePos = mouseCoords(ev);
+    $("#xxx").text (((mousePos.x-816)/6.8).toFixed(1));
+    $("#yyy").text (((mousePos.y-204.5)/4.5).toFixed(1));
+    return mousePos
+}
+function mouseCoords(ev)
+{
+    if(ev.pageX || ev.pageY){
+        return {x:ev.pageX, y:ev.pageY};
+    }
+    return{
+        x:ev.clientX + document.body.scrollLeft - document.body.clientLeft,
+        y:ev.clientY + document.body.scrollTop - document.body.clientTop
+    };
+}
 
 function onTrackedVideoFrame(currentTime, duration){
     $("#current").text(currentTime);
